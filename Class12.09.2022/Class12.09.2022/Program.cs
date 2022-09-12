fd2c39326fcb9de6caf0ae7c693e343d4d69d445
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2022-09-12 21:36:13 +0300

    CLassWork 12092022

---
+++ Class12.09.2022/Class12.09.2022/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class12._09._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TASKONE 
+            /* 
+             Random random = new Random();
+             int countOne = 0, countTwo = 0, countUniq = 0;
+             int[] massive = new int[20];
+             int[]uniq  = new int[20];
+             for(int i = 0;i < massive.Length;i++)
+             {
+                 massive[i] = random.Next(100);
+             }
+             for(int i = 0; i < massive.Length;i++  )
+             {
+                 if(massive[i]%2 == 0 || massive[i] == 0) { countOne++; }
+                 else { countTwo++; }
+                 Console.WriteLine(massive[i]);
+             }
+             for(int i = 0; i < massive.Length;i++  )
+             {
+                 foreach(int j in massive)
+                 {
+                     if(i == j)
+                     {
+                         countUniq++;
+                         if(countUniq>1)
+                         {
+                             countUniq = 0;
+                             uniq[i] = 0;
+                             break;
+                         }
+                         else { uniq[i] = 1; }
+                     }
+                 }
+             }
+                 Console.WriteLine("Количество четных элементов: {0}",countOne);
+                 Console.WriteLine("Количество нечетных элементов: {0}",countTwo);
+             for(int i = 0; i <= 20; i++) { if(uniq[i] == 1) { Console.WriteLine(i.ToString()); } } */
+            #endregion
+
+            #region TASKTWO
+            /*Random random = new Random();
+            int counter = 0;
+
+            int[] massive = new int[20];
+            for (int i = 0; i < massive.Length; i++)
+            {
+                massive[i] = random.Next(100);
+            }
+            Console.WriteLine("Введите число от 0 до 1000. Будут показаны все элементы меньше вашего числа.");
+            try 
+            {
+                string number = Console.ReadLine();
+                int z = Int32.Parse(number);
+                for(int i = 0; i < massive.Length; i++)
+                {
+                    if(massive[i] < z||massive[i] == 0)
+                    {
+                        counter++;
+                    }
+                }
+            Console.WriteLine("чисел меньше {0} найдено {1} штук",number, counter);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Только числа!");
+            }*/
+            #endregion
+            #region TASKTREE
+            int[] massive2 = new int[12] { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
+            int counter = 0;
+            string numbersText = "";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < massive2.Length; i++)
+            {
+                sb.Append(massive2[i]);
+            }
+            numbersText = sb.ToString();
+            Console.WriteLine("Массив для поиска числового вхождения: \n" + numbersText);
+            string searchText = Console.ReadLine();
+            for (int i = 0; i < numbersText.Length; i++)
+            {
+                if (numbersText.Contains(searchText))
+                {
+                    int indexContain = numbersText.IndexOf(searchText);
+                    counter++;
+                    numbersText = numbersText.Remove(0, indexContain + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Искомой подстроки не обнаружено!");
+                    break;
+                }
+            }
+            Console.WriteLine("Искомая подстрока найдена{0} раз(а) ", counter);
+            #endregion
+        }
+    }
+}
---
+++ MyFirstProjectOnCsharp/MyFirstProjectOnCsharp/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("It's easy to win forgiveness for being wrong; \n Being right is what gets you into real trouble.\n Bjarne Stroustrup");
+Console.WriteLine("Введите числа!");
+//List<String> list = new List<String> { };
+//for(int i = 0; i < list.Count; i++)
+//{
+//list[i] = Console.ReadLine();
+//}
+int n = 5;
+String[] list = new String[n];
+//Console.WriteLine(list);
+//for(int i = 0; i < n; i++)
+//{
+//    Console.WriteLine("Введите число для индекса {0}",i); 
+//    list[i] = Console.ReadLine();
+////}
+int[] massive = new int[5];
+for(int i = 0; i < massive.Length; i++)
+{
+    Console.WriteLine("Введите число для индекса {0}",i);
+    massive[i] = Int32.Parse(Console.ReadLine());
+}
+int summary = 0, multiply = 1, max , min ;
+min = massive[0];
+max = massive[0];
+
+for(int i = massive.Length - 1;i >0 ; i--)
+{
+    summary += massive[i];
+    multiply *= massive[i];
+    if (min < massive[i]) { min = massive[i]; };
+    if (max < massive[i]) { max = massive[i]; };
+}
+Console.WriteLine(max);
+Console.WriteLine(min);
+Console.WriteLine(multiply);
