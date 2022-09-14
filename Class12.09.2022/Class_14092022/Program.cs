7cffb861461fb2936e688641a426a2346a31c5e6
mr.serhioru@mail.ru <mr.serhioru@mail.ru>
2022-09-14 21:43:33 +0300

    Class and methods done

---
+++ Class12.09.2022/Class_14092022/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_14092022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int start,end;
+            start = Int32.Parse(Console.ReadLine());
+            end = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(MyClass.MyMethod(start,end));
+            Console.ReadLine(); */
+            /*int firstFib = 0, secondFib =1;
+            Fibonacci(firstFib, secondFib, Int32.Parse(Console.ReadLine()));*/
+            Plane pl = new Plane("Сутулов");
+            Plane pl1 = new Plane("Собаков", 2010);
+            Plane pl2 = new Plane("СобакоСутулов", 2022, "ТуполевыйСамолетостроительный", "Слаболетательный");
+            Console.WriteLine(pl.NameAndType());
+            Console.WriteLine(pl.NameAndType(true));
+            Console.WriteLine(pl.NameAndType(false));
+            Console.WriteLine(pl1.NameAndType());
+            Console.WriteLine(pl1.NameAndType(true));
+            Console.WriteLine(pl1.NameAndType(false));
+            Console.WriteLine(pl2.NameAndType());
+            Console.WriteLine(pl2.NameAndType(true));
+            Console.WriteLine(pl2.NameAndType(false));
+        }
+        /*static void Fibonacci(int a, int b, int endNumber)
+        {
+            int result = 0;
+            result = a + b;
+            if (result != endNumber && result < endNumber)
+            {
+                Fibonacci(b, result, endNumber);
+            }
+            else if (result == endNumber) 
+            {
+                Console.WriteLine("Это число Фибоначчи!");
+                Console.WriteLine("Число простое:{0}", Simple(result));
+            }
+            else
+            {
+                Console.WriteLine("Это не число Фибоначчи!");
+                Console.WriteLine("Число простое:{0}", Simple(result));
+            }
+        }
+        static bool Simple(int a)
+        {
+            if (a% 2 == 0||a%3==0||a%5==0||a%7==0||a%9==0) { return false; }
+            else { return true; }
+        }*/
+
+        
+
+    }
+    class Plane
+    {
+        public string _name { get; set; }
+        public string _manufacture { get; set; }
+        public int _year { get; set; }
+        public string _type { get; set; }
+        public Plane(string name)
+        {
+            _name = name;
+            _manufacture = "Аэрофлот";
+            _year = 2005;
+            _type = "Пассажирский";
+        }
+        public Plane(string name, int year)
+        {
+            _name = name;
+            _manufacture = "Аэрофлот";
+            _year = year;
+            _type = "Пассажирский";
+        }
+        public Plane(string name, int year, string manufucture, string type)
+        {
+            _name = name;
+            _manufacture = manufucture;
+            _year = year;
+            _type = type;
+        }
+        public string NameAndType()
+        {
+            return this._name + this._type;
+        }
+        public string NameAndType(bool manufacture)
+        {
+            if (manufacture) return this._name + this._type + this._manufacture;
+            else { return this._name + this._type; }
+
+        }
+
+
+        
+    }
+    /*class MyClass
+        {
+            public static int MyMethod(int start,int end )
+            {
+                int result = 1;
+                for(int i = start; i <= end; i++)
+                {
+                    result*=i;
+                }
+                return result;
+            }
+        }*/
+}
---
+++ Class12.09.2022/Class_14092022/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Class_14092022")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Class_14092022")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2acfc955-a147-4b94-a2d0-dab9e521e2d0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
