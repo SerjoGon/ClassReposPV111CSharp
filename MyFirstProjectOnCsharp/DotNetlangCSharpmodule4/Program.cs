9e020f4424510a11a81382530370bfeb52ae5814
SerjoGon <mr.serhioru@mail.ru>
2022-09-23 08:43:56 +0300

    Tic tac toe

---
+++ MyFirstProjectOnCsharp/DotNetlangCSharpmodule4/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNetlangCSharpmodule4
+{
+    internal class Program
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag = 0;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();// при повторе цикла игры очищает экран
+                Console.WriteLine("Игрок 1 : X и Игрок 2 : O");
+                Console.WriteLine("\n");
+                if (player % 2 == 0)//
+                {
+                    Console.WriteLine("Ход Игрока под №2");
+                }
+                else
+                {
+                    Console.WriteLine("Ход Игрока под №2");
+                }
+                Console.WriteLine("\n");
+                Board();
+                choice = int.Parse(Console.ReadLine());
+                if (arr[choice] != 'X' && arr[choice] != 'O')
+                {
+                    if (player % 2 == 0) 
+                    {
+                        arr[choice] = 'O';
+                        player++;
+                    }
+                    else
+                    {
+                        arr[choice] = 'X';
+                        player++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Извините позиция  {0} уже занята : {1}", choice, arr[choice]);
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Пару сек. я рисую новое поле!!!");
+                    Thread.Sleep(2000);
+                }
+                flag = CheckWin();
+            }
+            while (flag != 1 && flag != -1);
+            Console.Clear();
+            Board();
+            if (flag == 1)
+            {
+                Console.WriteLine("Игрок {0} победил!", (player % 2) + 1);
+            }
+            else
+            {
+                Console.WriteLine("Ничья, победила дружба!");
+            }
+            Console.ReadLine();
+        }
+
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+        private static int CheckWin()
+        {
+            #region Horzontal Winning Condtion
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            #endregion
+            #region Vertical Winning Condtion
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            #endregion
+            #region Diagonal Winning Condition
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            #endregion
+            #region Checking For Draw 
+            //проверка на ничью 
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            #endregion
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
