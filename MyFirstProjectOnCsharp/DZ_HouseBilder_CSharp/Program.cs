960f0d45e16b03bb248d0f2fdf37f983daa1503f
SerjoGon <mr.serhioru@mail.ru>
2022-09-30 16:07:29 +0300

    team bilders

---
+++ MyFirstProjectOnCsharp/DZ_HouseBilder_CSharp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_HouseBilder_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    interface IPart
+    {
+        string Name { get; set; }
+        string Material { get; set; }
+        bool Built { get; set; }
+    }
+    interface IWorker
+    {
+        string Name { set; get; }
+    }
+    class Basement : IPart
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public bool Built { get; set; }
+    }
+    class Walls : IPart
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public bool Built { get; set; }
+    }
+    class Door : IPart
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public bool Built { get; set; }
+    }
+    class Window : IPart
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public bool Built { get; set; }
+    }
+    class Roof : IPart
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public bool Built { get; set; }
+    }
+
+    class Worker : IWorker
+    {
+        public string Name { get; set; }
+    }
+    class TeamLeader:IWorker
+    {
+        public string Name { get; set;}
+    }
+    class Team
+    {
+
+    }
+}
