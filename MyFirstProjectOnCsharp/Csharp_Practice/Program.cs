583939da91781dbe58ac25a4f8403f28bdc18234
SerjoGon <mr.serhioru@mail.ru>
2022-09-30 16:17:11 +0300

    agfadfg

--- MyFirstProjectOnCsharp/Csharp_Practice/Program.cs
+++ MyFirstProjectOnCsharp/Csharp_Practice/Program.cs
@@ -11,31 +11,31 @@ namespace Csharp_Practice
         {
             //module2 Task2 
             #region Task2 
-            /*Random random = new Random();
-            int[,] massnumbers = new int[5,5];
-            for(int i = 0; i < 5; i++)
+            Random random = new Random();
+            int[,] massnumbers = new int[5, 5];
+            for (int i = 0; i < 5; i++)
             {
-                for(int j = 0; j < 5; j++)
+                for (int j = 0; j < 5; j++)
                 {
-                    massnumbers[i,j] = random.Next(-100,100);
+                    massnumbers[i, j] = random.Next(-100, 100);
                 }
             }
-            int min = massnumbers[0,0];
-            int max = massnumbers[0,0];
+            int min = massnumbers[0, 0];
+            int max = massnumbers[0, 0];
             int sum = 0;
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    if(min > massnumbers[i,j])min = massnumbers[i,j];
-                    if(max < massnumbers[i,j])max = massnumbers[i,j];
+                    if (min > massnumbers[i, j]) min = massnumbers[i, j];
+                    if (max < massnumbers[i, j]) max = massnumbers[i, j];
                 }
             }
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    if (massnumbers[i, j] >= min || massnumbers[i,j]<=max)sum += massnumbers[i,j] ;
+                    if (massnumbers[i, j] >= min || massnumbers[i, j] <= max) sum += massnumbers[i, j];
                 }
             }
 
@@ -43,59 +43,59 @@ namespace Csharp_Practice
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    Console.Write("{0}\t",massnumbers[i, j]);
+                    Console.Write("{0}\t", massnumbers[i, j]);
                 }
                 Console.Write("\n");
             }
-            Console.WriteLine("Сумма элементов массива между минимальным и максимальным значением:{0}",sum);
-            */
+            Console.WriteLine("Сумма элементов массива между минимальным и максимальным значением:{0}", sum);
+
             #endregion
 
             #region TASK3 Сaesar 's cipher
-            var cipher = new CeasarCipher();
-            Console.WriteLine("Напиши любой текст, я попробую его зашифровать!");
-            var message = Console.ReadLine();
-            Console.WriteLine("Введите ключ Шифра:");
-            var secretkey = Convert.ToInt32(Console.ReadLine());
-            var encryptedText = cipher.Encrypt(message, secretkey);
-            Console.WriteLine("Зашифрованный текст: {0}",encryptedText);
-            Console.WriteLine("Расшифрованный текст: {0}",cipher.Decrypt(encryptedText,secretkey));
+            //var cipher = new CeasarCipher();
+            //Console.WriteLine("Напиши любой текст, я попробую его зашифровать!");
+            //var message = Console.ReadLine();
+            //Console.WriteLine("Введите ключ Шифра:");
+            //var secretkey = Convert.ToInt32(Console.ReadLine());
+            //var encryptedText = cipher.Encrypt(message, secretkey);
+            //Console.WriteLine("Зашифрованный текст: {0}",encryptedText);
+            //Console.WriteLine("Расшифрованный текст: {0}",cipher.Decrypt(encryptedText,secretkey));
             #endregion
         }
 
     }
-    class CeasarCipher
-    {
-        const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        private string CodeEncode(string text, int k)
-        {
-            var fullAlphabet = alphabet + alphabet.ToLower();
-            var LetterLength = fullAlphabet.Length;
-            var cipherResult = "";
-            for(int i = 0; i < text.Length; i++)
-            {
-                var c = text[i];
-                var index = fullAlphabet.IndexOf(c);
-                if(index < 0)
-                {
-                    //если символ не знаком, оставляем его в исходном виде
-                    cipherResult += c.ToString();
-                }
-                else
-                {
-                    var codeIndex = (LetterLength + index + k)%LetterLength;
-                    cipherResult += fullAlphabet[codeIndex];
-                }
-            }
-            return cipherResult;
-        }
-        public string Encrypt(string Message, int key)
-        {
-            return CodeEncode(Message, key);
-        }
-        public string Decrypt(string encryptedMessage, int key)
-        {
-            return CodeEncode(encryptedMessage, -key);
-        }
-    }
+    //class CeasarCipher
+    //{
+    //    const string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    //    private string CodeEncode(string text, int k)
+    //    {
+    //        var fullAlphabet = alphabet + alphabet.ToLower();
+    //        var LetterLength = fullAlphabet.Length;
+    //        var cipherResult = "";
+    //        for(int i = 0; i < text.Length; i++)
+    //        {
+    //            var c = text[i];
+    //            var index = fullAlphabet.IndexOf(c);
+    //            if(index < 0)
+    //            {
+    //                //если символ не знаком, оставляем его в исходном виде
+    //                cipherResult += c.ToString();
+    //            }
+    //            else
+    //            {
+    //                var codeIndex = (LetterLength + index + k)%LetterLength;
+    //                cipherResult += fullAlphabet[codeIndex];
+    //            }
+    //        }
+    //        return cipherResult;
+    //    }
+    //    public string Encrypt(string Message, int key)
+    //    {
+    //        return CodeEncode(Message, key);
+    //    }
+    //    public string Decrypt(string encryptedMessage, int key)
+    //    {
+    //        return CodeEncode(encryptedMessage, -key);
+    //    }
+    //}
 }
---
+++ MyFirstProjectOnCsharp/Module2minmaxtask/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2minmaxtask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[,] B = new int[3, 4];
+            Random random = new Random();
+            int indexIminvalue = 0; int indexJminvalue = 0;
+            int indexImaxvalue = 0; int indexJmaxvalue = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    B[i, j] = random.Next(-10, 10);
+                }
+            }
+            int minvalue = B[0, 0]; int maxvalue = B[0, 0];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(B[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (minvalue > B[i, j]) { minvalue = B[i, j]; indexIminvalue = i; indexJminvalue = j; }
+                    if (maxvalue < B[i, j]) { maxvalue = B[i, j]; indexImaxvalue = i; indexJmaxvalue = j; }
+                }
+            }
+            Console.WriteLine($"MinValue:{ minvalue}по индексам{indexIminvalue}{indexJminvalue}");
+            Console.WriteLine($"MaxValue:{ maxvalue}по индексам{indexImaxvalue}{indexJmaxvalue}");
+            if (minvalue != maxvalue)
+            {
+                if ((indexIminvalue > indexImaxvalue && indexJminvalue > indexJmaxvalue) || (indexIminvalue > indexImaxvalue) || (indexIminvalue == indexImaxvalue && indexJminvalue > indexJmaxvalue))
+                {
+                    int temp = indexImaxvalue;
+                    indexImaxvalue = indexIminvalue;
+                    indexIminvalue = temp;
+                    temp = indexJmaxvalue;
+                    indexJmaxvalue = indexJminvalue;
+                    indexJminvalue = temp;
+                }
+                if ((indexImaxvalue == indexIminvalue && indexJmaxvalue - indexJminvalue == 1) || (indexImaxvalue - indexIminvalue == 1 && indexJminvalue == 3 && indexJmaxvalue == 0))
+                {
+                    sum = 0;
+                }
+                else
+                {
+                    for (int i = indexIminvalue; i <= indexImaxvalue; i++)
+                    {
+                        if (i == indexIminvalue && indexIminvalue == indexImaxvalue)
+                        {
+                            for (int j = indexJminvalue + 1; j < 4; j++)
+                            {
+                                sum += B[i, j];
+                            }
+                        }
+                        else if (i == indexIminvalue && indexIminvalue != indexImaxvalue)
+                        {
+                            for (int j = indexJminvalue + 1; j < 4; j++)
+                            {
+                                sum += B[i, j];
+                            }
+                        }
+                        else if (i == indexImaxvalue)
+                        {
+                            for (int j = 0 + 1; j < indexJmaxvalue; j++)
+                            {
+                                sum += B[i, j];
+                            }
+                        }
+                        else
+                        {
+                            for (int j = 0; j < 4; j++)
+                            {
+                                sum += B[i, j];
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine($"Сумма между минимальным и максиммальным элементом: {sum} ");
+
+            }
+            else
+            {
+                Console.WriteLine("Чудо 12 эелементов одинаковы!");
+            }
+        }
+    }
+}
